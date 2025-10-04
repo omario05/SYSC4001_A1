ee3b3749bd9902af2c2b9ae60810330001c308d9
Omar Elhogaraty <elhogaratyomar@gmail.com>
2025-10-04 19:35:17 -0400

    Update interrupts.cpp

--- interrupts.cpp
+++ interrupts.cpp
@@ -3,6 +3,8 @@
  * @file interrupts.cpp
  * @author Sasisekhar Govind
  *
+ * @author Omar Elhogaraty, 101302440
+ * @author Ahmad El-Jabi, 101303269
  */
 
 #include "interrupts.hpp"
@@ -14,16 +16,12 @@ int main(int argc, char** argv) {
     //the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
-
     std::string trace;      //!< string to store single line of trace file
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-    int x = 3;
-    std::cout << x;
-    std::cout << "thanks for helping";
-
-
+    long current_time = 0;
+    int context_save_time = 10;
     /******************************************************************/
 
     //parse each line of the input trace file
@@ -31,16 +29,28 @@ int main(int argc, char** argv) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity=="CPU"){
+            execution+=std::to_string(current_time)+", "+std::to_string(duration_intr)+", CPU Burst\n";
+            current_time+=duration_intr;
+        }
+        else if (activity=="SYSCALL"){
+            int device_num=duration_intr;
+            auto [interrupt_seq, updated_time]=intr_boilerplate(current_time, device_num, context_save_time, vectors);
+            execution+=interrupt_seq;
+            current_time=updated_time;
+        }
+        else if (activity=="END_IO"){
+            int device_num=duration_intr;
+            auto [interrupt_seq, updated_time]=intr_boilerplate(current_time, device_num, context_save_time, vectors);
+            execution+=interrupt_seq;
+            current_time=updated_time;    
+        }
 
-
-
+        
         /************************************************************************/
 
     }
-
     input_file.close();
-
     write_output(execution);
-
     return 0;
 }
